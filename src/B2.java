ff6e15176249e86ecc8f1dd1746dd7a6f39d702f
jiarui1991 <annhjr@gmail.com>
2017-03-04 13:29:06 -0800

    updated case

---
+++ src/B2.java
@@ -0,0 +1,5 @@
+
+
+public class B2 extends P implements A1, A2 {
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
@@ -13,15 +12,32 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         // creates an input stream for the file to be parsed
-        FileInputStream in = new FileInputStream("/Users/jiaruihu/Documents/workspace/Javapharser/src/A.java");
+        // FileInputStream in = new FileInputStream("/Users/jiaruihu/Documents/workspace/Javapharser/src/A.java");
 
         // parse the file
-        CompilationUnit cu = JavaParser.parse(in);
+        // CompilationUnit cu = JavaParser.parse(in);
 
         // prints the resulting compilation unit to default system output
-        System.out.println(cu.toString());
+        // System.out.println(cu.toString());
         // visit and print the methods names
-        new MethodVisitor().visit(cu, null);
+        // new MethodVisitor().visit(cu, null);
+
+        StringBuilder plantUmlSource = new StringBuilder();
+
+        plantUmlSource.append("@startuml\n");
+
+        plantUmlSource.append("Alice -> Bob: Authentication Request\n");
+
+        plantUmlSource.append("Bob --> Alice: Authentication Response\n");
+
+        plantUmlSource.append("@enduml");
+
+        SourceStringReader reader = new SourceStringReader(plantUmlSource.toString());
+
+        FileOutputStream output = new FileOutputStream(new File("/your/path/to/plantuml/test.svg"));
+
+        reader.generateImage(output, new FileFormatOption(FileFormat.SVG, false));
+
     }
 
     /**
