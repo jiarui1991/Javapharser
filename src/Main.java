7186ee1b632051851a8f47d2a7693f5c244ecb6b
Jiarui Hu <annhjr@gmail.com>
2017-04-01 23:01:04 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,41 @@
 //package com.github.javaparser;
-/*
- * public class Main {
- * public static void main(String[] args) throws Exception {
- * // creates an input stream for the file to be parsed
- * // FileInputStream in = new FileInputStream("/Users/jiaruihu/Documents/workspace/Javapharser/src/A.java");
- * // parse the file
- * // CompilationUnit cu = JavaParser.parse(in);
- * // prints the resulting compilation unit to default system output
- * // System.out.println(cu.toString());
- * // visit and print the methods names
- * // new MethodVisitor().visit(cu, null);
- * StringBuilder plantUmlSource = new StringBuilder();
- * plantUmlSource.append("@startuml\n");
- * plantUmlSource.append("Alice -> Bob: Authentication Request\n");
- * plantUmlSource.append("Bob --> Alice: Authentication Response\n");
- * plantUmlSource.append("@enduml");
- * // SourceStringReader reader = new SourceStringReader(plantUmlSource.toString());
- * FileOutputStream output = new FileOutputStream(new File("/your/path/to/plantuml/test.svg"));
- * // reader.generateImage(output, new FileFormatOption(FileFormat.SVG, false));
- * }
- */
+//comment
+  public class Main {
+  public static void main(String[] args) throws Exception {
+  // creates an input stream for the file to be parsed
+  // FileInputStream in = new FileInputStream("/Users/jiaruihu/Documents/workspace/Javapharser/src/A.java");
+  // parse the file
+  // CompilationUnit cu = JavaParser.parse(in);
+  // prints the resulting compilation unit to default system output
+  // System.out.println(cu.toString());
+  // visit and print the methods names
+  // new MethodVisitor().visit(cu, null);
+  StringBuilder plantUmlSource = new StringBuilder();
+  plantUmlSource.append("@startuml\n");
+  plantUmlSource.append("Alice -> Bob: Authentication Request\n");
+  plantUmlSource.append("Bob --> Alice: Authentication Response\n");
+  plantUmlSource.append("@enduml");
+  // SourceStringReader reader = new SourceStringReader(plantUmlSource.toString());
+  FileOutputStream output = new FileOutputStream(new File("/your/path/to/plantuml/test.svg"));
+  // reader.generateImage(output, new FileFormatOption(FileFormat.SVG, false));
+  }
+ 
     /**
      * Simple visitor implementation for visiting MethodDeclaration nodes.
      */
-/*
- * private static class MethodVisitor extends VoidVisitorAdapter<Void> {
- * @Override
- * public void visit(MethodDeclaration n, Void arg) {
- * /*
+
+  private static class MethodVisitor extends VoidVisitorAdapter<Void> {
+  @Override
+  public void visit(MethodDeclaration n, Void arg) {
+  /*
  * here you can access the attributes of the method.
  * this method will be called for all methods in this
  * CompilationUnit, including inner class methods
  */
-/*
- * System.out.println(n.getName());
-*
- * super.visit(n, arg);
- * }
- * }
- */
+
+  System.out.println(n.getName());
+
+  super.visit(n, arg);
+  }
+  }
+ 
