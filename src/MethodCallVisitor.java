5b2d4296d0525b0b3bf2f085c6d6beb53e6f4b58
Jiarui Hu <annhjr@gmail.com>
2017-04-01 23:01:53 -0700

    Update MethodCallVisitor.java

--- src/MethodCallVisitor.java
+++ src/MethodCallVisitor.java
@@ -14,10 +14,10 @@ public class MethodCallVisitor extends VoidVisitorAdapter {
     @Override
     public void visit(MethodCallExpr methodCallExpr, Object obj) {
 
-        System.out.println("Name of the Method Calling : " + methodCallExpr.getName());
         System.out.println("Args of the Method Calling : " + methodCallExpr.getArguments());
         System.out.println("Scope(Object of the name which is calling method) of the Method Calling : "
                 + methodCallExpr.getScope());
+        System.out.println("Name of the Method Calling : " + methodCallExpr.getName());
         System.out.println("Type Args of the Method Calling : " + methodCallExpr.getTypeArguments());
         System.out.println("---------------------------------------------------------");
 
