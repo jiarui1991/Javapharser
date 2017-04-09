63ce9609906dd16b22289c472de739214fc1e975
Jiarui Hu <annhjr@gmail.com>
2017-04-08 22:54:05 -0700

    Update umlparser.java

--- src/umlparser.java
+++ src/umlparser.java
@@ -1,1 +1,9 @@
 
+Class umlparser {
+  public static void main(String[] args) throws Exception {
+		// creates an input stream for the file to be parsed
+		FileInputStream in = new FileInputStream("/Users/hanchen/Dropbox/coding_interview/java/COMPE202_Personal_Project/test.java");
+
+		CompilationUnit compilationUnit = JavaParser.parse(in);
+	}
+}
