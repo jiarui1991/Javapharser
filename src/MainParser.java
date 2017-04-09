b2abd11cc03f9c53cb8ac830429d210740c919c5
Jiarui Hu <annhjr@gmail.com>
2017-04-08 22:44:57 -0700

    Update MainParser.java

--- src/MainParser.java
+++ src/MainParser.java
@@ -35,24 +35,24 @@ public class MainParser {
         File fileTemp[] = currentDirectory.listFiles((File pathName) -> pathName.getName().endsWith(".java"));
         CompilationUnit cUnit[] = new CompilationUnit[fileTemp.length];
 
-        try {
-            // parse all JAVA files
-            for (int index = 0; index < cUnit.length; index++) {
-                cUnit[index] = JavaParser.parse(fileTemp[index]);
-            }
-        } catch (Exception e) {
-            System.out.println("Exception Occurred :: " + e.getMessage());
-            e.printStackTrace();
-        }
+//         try {
+//             // parse all JAVA files
+//             for (int index = 0; index < cUnit.length; index++) {
+//                 cUnit[index] = JavaParser.parse(fileTemp[index]);
+//             }
+//         } catch (Exception e) {
+//             System.out.println("Exception Occurred :: " + e.getMessage());
+//             e.printStackTrace();
+//         }
 
-        for (; StaticClass.index < cUnit.length; StaticClass.index++) {
+//         for (; StaticClass.index < cUnit.length; StaticClass.index++) {
            
-            new ConstructorVisitor().visit(cUnit[StaticClass.index], null);
-             new ClassVisitor().visit(cUnit[StaticClass.index], null);
-            new FieldVisitor().visit(cUnit[StaticClass.index], null);
-            new MethodVisitor().visit(cUnit[StaticClass.index], null);
-            new MethodCallVisitor().visit(cUnit[StaticClass.index], null);
-        }
+//             new ConstructorVisitor().visit(cUnit[StaticClass.index], null);
+//              new ClassVisitor().visit(cUnit[StaticClass.index], null);
+//             new FieldVisitor().visit(cUnit[StaticClass.index], null);
+//             new MethodVisitor().visit(cUnit[StaticClass.index], null);
+//             new MethodCallVisitor().visit(cUnit[StaticClass.index], null);
+//         }
 
         // Creating the UML File
 
@@ -70,17 +70,17 @@ public class MainParser {
         String end = "\n@enduml";
 
         // Temporary Model Variables
-        FieldModel tempFieldModel = new FieldModel();
-        ConstructorModel tempConstructorModel = new ConstructorModel();
-        MethodModel tempMethodModel = new MethodModel();
-        ClassModel tempClassModel = new ClassModel();
+//         FieldModel tempFieldModel = new FieldModel();
+//         ConstructorModel tempConstructorModel = new ConstructorModel();
+//         MethodModel tempMethodModel = new MethodModel();
+//         ClassModel tempClassModel = new ClassModel();
 
         // Temporary List Variables
-        ArrayList<MethodModel> tempMethodList = new ArrayList<MethodModel>();
-        ArrayList<String> tempImplementsList = new ArrayList<String>();
-        ArrayList<String> tempExtendsList = new ArrayList<String>();
-        ArrayList<FieldModel> tempFieldList = new ArrayList<FieldModel>();
-        ArrayList<ConstructorModel> tempConstructorList = new ArrayList<ConstructorModel>();
+//         ArrayList<MethodModel> tempMethodList = new ArrayList<MethodModel>();
+//         ArrayList<String> tempImplementsList = new ArrayList<String>();
+//         ArrayList<String> tempExtendsList = new ArrayList<String>();
+//         ArrayList<FieldModel> tempFieldList = new ArrayList<FieldModel>();
+//         ArrayList<ConstructorModel> tempConstructorList = new ArrayList<ConstructorModel>();
         
 
         // Getting all Class Names in an ArrayList<String>
