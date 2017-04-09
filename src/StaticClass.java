ef25bb5add92bb23e002d8271f571e5e850b3b1c
Jiarui Hu <annhjr@gmail.com>
2017-04-08 22:49:17 -0700

    Update StaticClass.java

--- src/StaticClass.java
+++ src/StaticClass.java
@@ -1,16 +1,10 @@
 import java.util.ArrayList;
 
-/**
- * 
- */
-
-/**
- * @author Darshil
- *
- */
 public class StaticClass {
-    public static ArrayList<ClassModel> classList = new ArrayList<ClassModel>();
-    public static int index;
+    /* Testing on static class
+//     public static ArrayList<ClassModel> classList = new ArrayList<ClassModel>();
+//     public static int index;
+    */
     /*
      * public static ArrayList<FieldModel> fieldList = new ArrayList<FieldModel>();
      * public static ArrayList<ConstructorModel> constructorList = new ArrayList<ConstructorModel>();
